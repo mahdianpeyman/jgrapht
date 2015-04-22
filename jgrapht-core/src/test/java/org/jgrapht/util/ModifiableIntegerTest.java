aabde6c7146295a49daa03b10ed9244e298f252b
Peyman Mahdian <mahdianpeyman@gmail.com>
2015-04-22 00:22:55 -0500

    23 more new tests

--- jgrapht-core/src/test/java/org/jgrapht/util/ModifiableIntegerTest.java
+++ jgrapht-core/src/test/java/org/jgrapht/util/ModifiableIntegerTest.java
@@ -12,9 +12,14 @@ public class ModifiableIntegerTest
 {
     //~ Methods ----------------------------------------------------------------
 
+    Integer I1 = 1;
     int i0 = 0;
     int i1 = 1;
-    ModifiableInteger mi0 = new ModifiableInteger(0);
+    double d1 = 1.0;
+    float f1 = 1.0f;
+    long l1 = 1L;
+    ModifiableInteger mi0 = new ModifiableInteger(i0);
+    ModifiableInteger mi1 = new ModifiableInteger(i1);
 
     public void testsetGetValue()
     {
@@ -34,6 +39,66 @@ public class ModifiableIntegerTest
         mi0.setValue(i1);
         mi0.decrement();
         assertEquals(i1 - 1, mi0.getValue());
+    }
+
+    public void testcompareTo1()
+    {
+        assertEquals(-1, mi0.compareTo(mi1));
+    }
+
+    public void testcompareTo2()
+    {
+        assertEquals(1, mi1.compareTo(mi0));
+    }
+
+    public void testcompareTo3()
+    {
+        assertEquals(0, mi0.compareTo(mi0));
+    }
+
+    public void testequals1()
+    {
+        assertTrue(mi0.equals(mi0));
+    }
+
+    public void testequals2()
+    {
+        assertFalse(mi0.equals(mi1));
+    }
+
+    public void testequals3()
+    {
+        assertFalse(mi0.equals(i1));
+    }
+
+    public void testdoubleValue()
+    {
+        assertEquals(d1, mi1.doubleValue());
+    }
+
+    public void testfloatValue()
+    {
+        assertEquals(f1, mi1.floatValue());
+    }
+
+    public void testintValue()
+    {
+        assertEquals(i1, mi1.intValue());
+    }
+
+    public void testlongValue()
+    {
+        assertEquals(l1, mi1.longValue());
+    }
+
+    public void testtoInteger()
+    {
+        assertEquals(I1, mi1.toInteger());
+    }
+
+    public void testtoString()
+    {
+        assertEquals(I1.toString(), mi1.toString());
     }
     
 
