6174f8848a2e05759797d55f6a1763083226e817
Peyman Mahdian <mahdianpeyman@gmail.com>
2015-04-21 22:22:56 -0500

    2 more new tests

--- jgrapht-core/src/test/java/org/jgrapht/util/VertexPairTest.java
+++ jgrapht-core/src/test/java/org/jgrapht/util/VertexPairTest.java
@@ -12,13 +12,27 @@ public class VertexPairTest
     //~ Methods ----------------------------------------------------------------
     String s1 = "A";
     String s2 = "B";
+    String s3 = "C";
+    VertexPair<String> vp1 = new VertexPair<String>(s1 , s2);
 
     public void testgetFirstAndSecond()
     {
-        VertexPair<String> vp1 = new VertexPair<String>(s1 , s2);
-
         assertEquals(s1, vp1.getFirst());
         assertEquals(s2, vp1.getSecond());
+    }
+
+    public void testhasVertex()
+    {
+        assertTrue(vp1.hasVertex(s1));
+        assertTrue(vp1.hasVertex(s2));
+        assertFalse(vp1.hasVertex(s3));
+    }
+
+    public void testgetOther()
+    {
+        assertEquals(s1, vp1.getOther(s2));
+        assertEquals(s2, vp1.getOther(s1));
+        assertEquals(null, vp1.getOther(s3));
     }
 }
 
