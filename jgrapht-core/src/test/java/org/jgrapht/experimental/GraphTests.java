44e35bb7a0d5583a1642c480f36637db7390c17e
unknown <alebios2@SIEBL-4303-06.ad.uillinois.edu>
2015-04-21 23:40:15 -0500

    bug in GraphSquare.java

---
+++ jgrapht-core/src/test/java/org/jgrapht/experimental/GraphTests.java
@@ -0,0 +1,50 @@
+package org.jgrapht.experimental;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.jgrapht.*;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+
+import junit.framework.TestCase;
+
+public class GraphSquareTest     
+	extends TestCase
+{
+    
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * @see junit.framework.TestCase#TestCase(java.lang.String)
+     */
+    public GraphSquareTest(String name)
+    {
+        super(name);
+    }
+    
+
+    /**
+     * Bug java.lang.IllegalArgumentException: must be instance of either DirectedGraph or UndirectedGraph
+     * the graph is of type DirectedGraph, however, I still obtain the IllegalArgumentException
+     */
+    public void testGraphSquare()
+    {
+        GraphSquare<Integer, DefaultEdge> Gsquare;		
+        DirectedGraph<Integer, DefaultEdge> g2 =
+		    new DefaultDirectedGraph<Integer, DefaultEdge>(
+		        DefaultEdge.class);
+		g2.addVertex(0);
+		g2.addVertex(1);
+		g2.addVertex(2);
+		g2.addEdge(0, 1);
+		g2.addEdge(0, 2);
+		
+		//Gsquare = new GraphSquare<Integer, DefaultEdge>(g2, false);
+		
+       // assertEquals(g2, Gsquare);
+
+		     
+    }
+
+}
