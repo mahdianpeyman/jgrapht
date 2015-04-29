bf3c8e8833f3b0b89ca0b1e1e1938a5dd3899b0e
Peyman Mahdian <mahdianpeyman@gmail.com>
2015-04-29 13:43:04 -0500

    BUG in Fibonucci Heap

--- jgrapht-core/src/test/java/org/jgrapht/util/FibonacciHeapTest.java
+++ jgrapht-core/src/test/java/org/jgrapht/util/FibonacciHeapTest.java
@@ -100,18 +100,84 @@ public class FibonacciHeapTest
         }
         return fh;
     }
+    public Double getMin(FibonacciHeap<Integer> fh1, FibonacciHeap<Integer> fh2)
+    {
+        if(fh1.isEmpty() && fh2.isEmpty())
+            return Double.NaN;
+        if(fh1.isEmpty())
+            return fh2.min().getKey();
+        if(fh2.isEmpty())
+            return fh1.min().getKey();
+        return  Math.min(fh1.min().getKey(), fh2.min().getKey());
+    }
+
+    public Double removeMin(FibonacciHeap<Integer> fh1, FibonacciHeap<Integer> fh2)
+    {
+        if(fh1.isEmpty() && fh2.isEmpty())
+            return Double.NaN;
+        if(fh1.isEmpty())
+            return fh2.removeMin().getKey();
+        if(fh2.isEmpty())
+            return fh1.removeMin().getKey();
+        if(fh1.min().getKey() < fh2.min().getKey())
+            return fh1.removeMin().getKey();
+        return fh2.removeMin().getKey();
+    }
+
+    public void assertMinUnion(FibonacciHeap<Integer> fh1, FibonacciHeap<Integer> fh2, FibonacciHeap<Integer> fh3)
+    {
+        while (!fh1.isEmpty() || !fh2.isEmpty())
+        {
+            assertFalse(fh3.isEmpty());
+            assertEquals(removeMin(fh1, fh2), fh3.removeMin().getKey());
+            assertEquals(fh1.size() + fh2.size(), fh3.size());
+        }
+        assertTrue(fh3.isEmpty());
+    }
 
     public void unionPUT(int size1, int size2)
     {
-        FibonacciHeap<Integer> fb1 = getRandomFibHeapInteger(size1);
-        FibonacciHeap<Integer> fb2 = getRandomFibHeapInteger(size2);
-        FibonacciHeap<Integer> fb3 = FibonacciHeap.union(fb1, fb2);
-        double min = Math.min(fb1.min().getKey(), fb2.min().getKey());
-        assertEquals(min, fb3.min().getKey());
+        FibonacciHeap<Integer> fh1 = getRandomFibHeapInteger(size1);
+        FibonacciHeap<Integer> fh2 = getRandomFibHeapInteger(size2);
+        FibonacciHeap<Integer> fh3 = FibonacciHeap.union(fh1, fh2);
+        double min = getMin(fh1, fh2);
+        if(size1 + size2 > 0)
+            assertEquals(min, fh3.min().getKey());
+        assertEquals(size1, fh1.size());
+        assertEquals(size2, fh2.size());
+        assertEquals(size1 + size2, fh3.size());
+        //assertMinUnion(fh1, fh2, fh3);
     }
 
     public void testUnion(){
+        unionPUT(0, 0);
+        unionPUT(0, 1);
+        unionPUT(1, 0);
         unionPUT(1, 1);
+        unionPUT(0, 2);
+        unionPUT(0, 1000);
+        unionPUT(1, 1000);
+        unionPUT(2, 1000);
+        unionPUT(1000, 1000);
+    }
+
+
+    public void testEmptySize() 
+    {
+        FibonacciHeap<Integer> fh1 = new FibonacciHeap<Integer>();
+        FibonacciHeap<Integer> fh2 = new FibonacciHeap<Integer>();
+        FibonacciHeapNode<Integer> fn1 = new FibonacciHeapNode<Integer>(0);
+        FibonacciHeapNode<Integer> fn2 = new FibonacciHeapNode<Integer>(0);
+
+        fh1.insert(fn1, 0.0);
+        fh2.insert(fn2, 0.1);
+
+        FibonacciHeap<Integer> fh3 = FibonacciHeap.union(fh1, fh2);
+
+        fh1.removeMin();
+
+        assertEquals(0, fh1.size());
+        assertTrue(fh1.isEmpty());
     }
 
 
